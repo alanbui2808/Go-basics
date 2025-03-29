5ca05449f4b11e0bf7ff548d748e6deacb506cc6
Alan <qhbui@umass.edu>
2025-03-28 19:16:46 -0500

    implement the Shape interface with getArea method

--- HTTPs/Shape/main.go
+++ HTTPs/Shape/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import "fmt"
 
+type Triangle struct {
+	height float64
+	base   float64
+}
+
+type Square struct {
+	sideLenght float64
+}
+
+type Shape interface {
+	printArea() float64
+}
+
+func main() {
+	t := Triangle{height: 10, base: 10}
+	s := Square{sideLenght: 10}
+
+	fmt.Println("Area of the triangle:", t.getArea())
+	fmt.Println("Area of the square:", s.getArea())
+}
+
+func (t *Triangle) getArea() float64 {
+	height, base := t.height, t.base
+
+	return 0.5 * base * height
+}
+
+func (s *Square) getArea() float64 {
+	sl := s.sideLenght
+
+	return sl * sl
 }
